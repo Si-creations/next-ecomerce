2f48fbb1d3456f3873ce543e1563afb9bfa390d2
Si-creations <s.mrocek@gmail.com>
2023-11-26 21:14:03 +0100

    adding webhooks

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,45 @@
-export default function Dashboard() {
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+const fetchOrders = async () => {
+  const prisma = new PrismaClient();
+  //Get user
+  const user = await getServerSession(authOptions);
+  if (!user) {
+    return null;
+  }
+  const orders = await prisma.order.findMany({
+    where: { userId: user?.user?.id },
+    include: {
+      products: true,
+    },
+  });
+  return orders;
+};
+
+export default async function Dashboard() {
+  const orders = await fetchOrders();
+  if (orders === null)
+    return <div>You need to be logged in to view your orders</div>;
+  if (orders.length === 0) {
+    return (
+      <div>
+        <h1>No orders placed</h1>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>Dashboard</h1>
+      <div className="font-medium">
+        {orders.map((order) => (
+          <div key={order.id} className="rounded-lg">
+            <h2>Order reference: {order.id}</h2>
+            <p>Time: {new Date(order.createDate)}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+                 
